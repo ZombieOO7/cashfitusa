b1c1ff10138a387f5035ab04bf0f873db7f09813
wc.arajan <arajan@webcluesinfotech.com>
2021-02-13 14:21:13 +0530

    changes

--- resources/views/frontend/sections/help.blade.php
+++ resources/views/frontend/sections/help.blade.php
@@ -1,127 +1,78 @@
-<section class="loan-process-section here-help section-padding"  style="background-color:#fff;">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <div class="section_title text-center mb-40">
-                    <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">We are Here for You!</h3>
-                    <span class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s" style="background: #000 !important;"></span>
-                    <p class="border-none">You are not alone in these difficult times. We are here to ease your burden amidst the financial crisis with our quick and easy-approved loans.</p>
+<div class="loanpagemain">
+    <section class="loan-process-section here-help section-padding"  style="background-color:#fff;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="section_title text-center mb-40">
+                        <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">We are Here for You!</h3>
+                        <span class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s" style="background: #000 !important;"></span>
+                        <p class="border-none">You are not alone in these difficult times. We are here to ease your burden amidst the financial crisis with our quick and easy-approved loans.</p>
+                    </div>
+                </div>
+            </div>
+
+                    <div class="row process-list">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
+                    <div class="single-process">
+                        <div class="proces-icon" style="border-radius: 10px;">
+                            {{-- <span class="pe-7s-note"></span> --}}
+                            <img src="{{asset('frontend/img/about/clockicon.png')}}" alt="">
+                        </div>
+                        <div class="content pl-1">
+                            <h4 class="process-title">Round the Clock</h4>
+                            <p>We provide you the flexibility to apply anytime from anywhere. No need to wait for a local lender or worry about the timezone. Just log in to CashFitUSA and get your loan credited.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
+                    <div class="single-process">
+                        <div class="proces-icon" style="border-radius: 10px;">
+                            {{-- <span class="pe-7s-id"></span> --}}
+                            <img src="{{asset('frontend/img/about/hereicon2.png')}}" alt="">
+                        </div>
+                        <div class="content">
+                            <h4 class="process-title">Simplified Application to Approval Process</h4>
+                            <p>Don’t worry about putting together a file as thick as a home in Japan. If you can read this, most probably, you can easily apply for a loan on our platform. And, our technology will take it from there. Dreaming and achieving those dreams has never been this easy.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
+                    <div class="single-process">
+                        <div class="proces-icon" style="border-radius: 10px;">
+                            {{-- <span class="pe-7s-check"></span> --}}
+                            <img src="{{asset('frontend/img/about/hereicon3.png')}}" alt="">
+                        </div>
+                        <div class="content">
+                            <h4 class="process-title">Transparent Fees Structures</h4>
+                            <p>You can take up our loan agreement and count the number of asterisks, because there are hardly any. There are no upfront payments and barely any fees. All the fees, even the ones that come in decimals, will be explained to you in absolute detail.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
+                    <div class="single-process">
+                        <div class="proces-icon" style="border-radius: 10px;">
+                            {{-- <span class="pe-7s-cash"></span> --}}
+                            <img src="{{asset('frontend/img/about/hereicon4.png')}}" alt="">
+                        </div>
+                        <div class="content">
+                            <h4 class="process-title">Data Privacy</h4>
+                            <p>Your data is also an asset. Hence, we have put together a comprehensive enterprise-scale data infrastructure to secure your data from any potential threats or leaks.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
+                    <div class="single-process">
+                        <div class="proces-icon" style="border-radius: 10px;">
+                            {{-- <span class="pe-7s-cash"></span> --}}
+                            <img src="{{asset('frontend/img/about/hereicon5.png')}}" alt="">
+                        </div>
+                        <div class="content">
+                            <h4 class="process-title">A Team that Loves to Help!</h4>
+                            <p>Believe it or not – we are in the business of helping you achieve whatever you have dreamt. When you achieve it, our loans get paid back, and everyone gets to sleep with a smile. This is why our entire team is happy to help, wherever and whenever you need us.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-                <div class="row process-list">
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
-                <div class="single-process">
-                    <div class="proces-icon" style="border-radius: 10px;">
-                        {{-- <span class="pe-7s-note"></span> --}}
-                        <img src="{{asset('frontend/img/about/clockicon.png')}}" alt="">
-                    </div>
-                    <div class="content pl-1">
-                        <h4 class="process-title">Round the Clock</h4>
-                        <p>We provide you the flexibility to apply anytime from anywhere. No need to wait for a local lender or worry about the timezone. Just log in to CashFitUSA and get your loan credited.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
-                <div class="single-process">
-                    <div class="proces-icon" style="border-radius: 10px;">
-                        {{-- <span class="pe-7s-id"></span> --}}
-                        <img src="{{asset('frontend/img/about/hereicon2.png')}}" alt="">
-                    </div>
-                    <div class="content">
-                        <h4 class="process-title">Simplified Application to Approval Process</h4>
-                        <p>Don’t worry about putting together a file as thick as a home in Japan. If you can read this, most probably, you can easily apply for a loan on our platform. And, our technology will take it from there. Dreaming and achieving those dreams has never been this easy.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
-                <div class="single-process">
-                    <div class="proces-icon" style="border-radius: 10px;">
-                        {{-- <span class="pe-7s-check"></span> --}}
-                        <img src="{{asset('frontend/img/about/hereicon3.png')}}" alt="">
-                    </div>
-                    <div class="content">
-                        <h4 class="process-title">Transparent Fees Structures</h4>
-                        <p>You can take up our loan agreement and count the number of asterisks, because there are hardly any. There are no upfront payments and barely any fees. All the fees, even the ones that come in decimals, will be explained to you in absolute detail.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
-                <div class="single-process">
-                    <div class="proces-icon" style="border-radius: 10px;">
-                        {{-- <span class="pe-7s-cash"></span> --}}
-                        <img src="{{asset('frontend/img/about/hereicon4.png')}}" alt="">
-                    </div>
-                    <div class="content">
-                        <h4 class="process-title">Data Privacy</h4>
-                        <p>Your data is also an asset. Hence, we have put together a comprehensive enterprise-scale data infrastructure to secure your data from any potential threats or leaks.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12 process text-center">
-                <div class="single-process">
-                    <div class="proces-icon" style="border-radius: 10px;">
-                        {{-- <span class="pe-7s-cash"></span> --}}
-                        <img src="{{asset('frontend/img/about/hereicon5.png')}}" alt="">
-                    </div>
-                    <div class="content">
-                        <h4 class="process-title">A Team that Loves to Help!</h4>
-                        <p>Believe it or not – we are in the business of helping you achieve whatever you have dreamt. When you achieve it, our loans get paid back, and everyone gets to sleep with a smile. This is why our entire team is happy to help, wherever and whenever you need us.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-<!--         <div class="row process-list">
-            <div class="col-lg-3 col-md-6 process text-center">
-                <div class="help-process">
-                    <div class="proces-icon">
-                        <img src="{{asset('frontend/img/customer.svg')}}" alt="">
-                        {{-- <span class="pe-7s-note"></span> --}}
-                    </div>
-                    <div class="content">
-                        <p>Yes. We are 100% real. Our team has experienced the hassles of long and arduous loan processes. The problem with most loan agreements is that they are inhumane. Even to get a loan in an emergency, you have to go through the same time-consuming loops of documentation, approvals, and hidden charges. By the time you reach the end, either the time is lost or the loan has become way too expensive for your pocket.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 process text-center">
-                <div class="help-process">
-                    <div class="proces-icon">
-                        <img src="{{asset('frontend/img/device.svg')}}" alt="">
-                        {{-- <span class="pe-7s-id"></span> --}}
-                    </div>
-                    <div class="content">
-                        <p>Since we have felt that burn as consumers, we wanted to create something that would be miles ahead of the market. Our application-to-approval technology takes care of all the data aggregation, so you don’t have to worry about the paperwork. If you carry a decent credit score, getting a loan of $2,000-$8,000 would be a process that doesn’t last more than a few minutes.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 process text-center">
-                <div class="help-process">
-                    <div class="proces-icon">
-                        <img src="{{asset('frontend/img/secure_cyber.svg')}}" alt="">
-                        {{-- <span class="pe-7s-check"></span> --}}
-                    </div>
-                    <div class="content">
-                        <p>Our fast loan decision process will help you explore other avenues in case your credit score is not eligible for the amount you seek. We are not loan sharks. We are just a friend who has experienced what you are experiencing right now, and wants to help you financially when you need it the most.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 process text-center">
-                <div class="help-process">
-                    <div class="proces-icon">
-                        <img src="{{asset('frontend/img/secure_cyber.svg')}}" alt="">
-                        {{-- <span class="pe-7s-check"></span> --}}
-                    </div>
-                    <div class="content">
-                        <p>Loans work on trust. And we are here, extending our hand, to build that relationship of trust with you.</p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
-
-
-
-    </div>
-</section>
+    </section>
+</div>
